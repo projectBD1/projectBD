b2fa924a03d0a94766e91d13ffadc6118afca719
MystProgrammer <animeshdeb89@gmail.com>
2025-01-10 15:23:40 -0500

    Fix lint issues

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 import express from 'express'
-const app=express()
-app.get("/", (req,res)=>{
-    res.send("Hello woorld.")
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Hello woorld.')
 })
-app.listen(3000,()=>{
-    console.log("Listening on port 3000")
+app.listen(3000, ()=>{
+  console.log('Listening on port 3000')
 })
