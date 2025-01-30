61427d5fed47ab24e464534aa5096caa02bce104
Andy Zhou <azhou7@buffalo.edu>
2025-01-30 01:49:20 -0500

    Added basic Report-Form functionality

---
+++ client/src/app/components/UploadForm.jsx
@@ -0,0 +1,61 @@
+'use client';
+import React, {useState, useEffect} from 'react';
+
+function UploadForm(){
+
+    const defaultFormValues = {
+        location: "",
+        date: "",
+        description: "",
+        source: "",
+    }
+
+    const [formValues, setFromValues] = useState(defaultFormValues)
+
+    const submitForm = () => {
+        fetch("http://localhost:3000/uploadreport", {
+            method: "POST",
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formValues)
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log("POST Response:", response);
+            setFromValues(defaultFormValues)
+        })
+        .catch(error => {
+            console.log("Error:", error)
+        });
+    };
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setFromValues((prevValues) => ({
+            ...prevValues,
+            [name]: value,
+        }))
+    }
+
+    const defaultF = () => {
+        setFromValues(defaultFormValues)
+    }
+
+    return(
+        <>
+        <form className="UploadForm" onSubmit={(e) => {e.preventDefault(); submitForm();}}>
+            <input type='text' name="location" value={formValues.location} onChange={handleChange} placeholder='Input Location of Violence'></input>
+            <br/>
+            <input type='date' name='date' value={formValues.date} onChange={handleChange}></input>
+            <br/>
+            <textarea name="description" value={formValues.description} onChange={handleChange} placeholder='Input Description of Violence'></textarea>
+            <br/>
+            <input type='text' name='source' value={formValues.source} onChange={handleChange} placeholder='Optional: Input a source'></input>
+            <br/>
+            <button>Submit</button>
+        </form>
+        </>
+    )
+} export default UploadForm;
+
--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+import mongoose from 'mongoose';
+import BDSS_MAP from './model/BDSS_MAP.js';
+
+mongoose.connect("")
+
 app.get('/', (req, res) => {
   res.send('Animesh is a banana.')
 })
 app.listen(3000, () => {
   console.log('Listening on port 3000')
+})
+
+app.post('/uploadreport', (req, res) => {
+  console.log(req.body)
+
+  const BD_Report = new BDSS_MAP({
+    location: req.body.location,
+    date: req.body.date,
+    description: req.body.description,
+    source1: req.body.source,
+  })
+
+  BD_Report.save()
+
+  res.status(200).json("woot")
 })
