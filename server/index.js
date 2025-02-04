abb224a89d4c02ae6988ae2990ca863e060c1dbb
MystProgrammer <animeshdeb89@gmail.com>
2025-02-04 17:16:49 -0500

    Linter fix

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 app.use(cors({
 
-  origin:'http://localhost:3001',
-  credentials:true
+  origin: 'http://localhost:3001',
+  credentials: true
 }))
 app.use(express.json())
 
