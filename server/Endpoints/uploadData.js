0324d9e7b30ae2399d98a642207e00afc55086f7
MystProgrammer <animeshdeb89@gmail.com>
2025-02-04 17:13:07 -0500

    Linter fix

--- server/Endpoints/uploadData.js
+++ server/Endpoints/uploadData.js
@@ -1,32 +1,30 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import BDSS_MAP from '../model/BDSS_MAP.js';
+import express from 'express'
+import mongoose from 'mongoose'
+import BDSS_MAP from '../model/BDSS_MAP.js'
 
-const router = express.Router();
+const router = express.Router()
 
 router.post('/', async (req, res) => {
-  try{
-    console.log(req.body);
-    console.log('connection string: ', process.env.CONNECTION_STRING);
-  const connection=await mongoose.connect(process.env.CONNECTION_STRING);
+  try {
+    console.log(req.body)
+    console.log('connection string: ', process.env.CONNECTION_STRING)
+    await mongoose.connect(process.env.CONNECTION_STRING)
 
-  const bdReport = await BDSS_MAP.create({
-    location: req.body.location,
-    date: req.body.date,
-    description: req.body.description,
-    source1: req.body.source,
-  });
-  console.log("bd report: ", bdReport)
+    const bdReport = await BDSS_MAP.create({
+      location: req.body.location,
+      date: req.body.date,
+      description: req.body.description,
+      source1: req.body.source
+    });
+    console.log("bd report: ", bdReport)
 
 
 
-  res.status(200).json({ data: 'Woot' });
+    res.status(200).json({ data: 'Woot' })
 
-  mongoose.disconnect();
-  }catch(err){
-    console.error(err)
-  }
+    mongoose.disconnect()
+  } catch (err) {
+    console.error(err)}
   
-});
-
-export default router;
+})
+export default router
