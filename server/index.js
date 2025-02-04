50e3d1ed664731bf11aab67fa6e1c4835a944b45
MystProgrammer <animeshdeb89@gmail.com>
2025-02-04 17:06:19 -0500

    Refactor endpoints

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-const app = express();
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import BDSS_MAP from './model/BDSS_MAP.js'
+import dotenv from 'dotenv'
+import uploadData from "./Endpoints/uploadData.js"
+
+dotenv.config()
 
 app.use(cors());
 app.use(express.json());
 
-import mongoose from 'mongoose';
-import BDSS_MAP from './model/BDSS_MAP.js';
+app.use(cors({
+  origin:'http://localhost:3001',
+  credentials:true,
+}))
+app.use(express.json())
 
-mongoose.connect("")
+
 
 app.get('/', (req, res) => {
   res.send('Animesh is a banana.')
@@ -16,18 +24,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
+app.use('/uploadreport', uploadData)
 
-app.post('/uploadreport', (req, res) => {
-  console.log(req.body)
-
-  const bdReport = new BDSS_MAP({
-    location: req.body.location,
-    date: req.body.date,
-    description: req.body.description,
-    source1: req.body.source,
-  })
-
-  bdReport.save()
-
-  res.status(200).json("woot")
-})
