585bfad0e753c2a9a66d517ae7349c2f41264d31
MystProgrammer <animeshdeb89@gmail.com>
2025-02-06 17:10:36 -0500

    Update comment

--- server/Middleware/rateLimiter.js
+++ server/Middleware/rateLimiter.js
@@ -4,8 +4,8 @@ const Limiter=rateLimit({
 windowMs: 15*60*1000,//15 mins
 max:5,//number of reqs
 message:"Please try again later.",
-standardHeaders:false,
-legacyHeaders:false,
+standardHeaders:false,//sending number of rate limits remaining in header, set to false so we don't send
+legacyHeaders:false,//legacyHeaders set to false, older version of standardHeaders
 });
 
 export default Limiter
