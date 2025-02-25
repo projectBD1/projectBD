0106bc36a4192b39934eabcfb1cd55c61fcacb53
Andy Zhou <azhou7@buffalo.edu>
2025-02-24 20:19:13 -0500

    Homepage implemented mostly

---
+++ client/src/app/components/Statistics.jsx
@@ -0,0 +1,23 @@
+import './Statistics.css'
+const Statistics = () => {
+    return(
+        <div className='statistics'>
+            <h1>What is Happening In Bangladesh?</h1>
+            <div className='stat-box-container'>
+                <div className='stat-box'>
+                    <h1>1/10 of bananas get eaten</h1>
+                </div>
+                <div className='stat-box'>
+                    <h1>2/10 of bananas get eaten</h1>
+                </div>
+                <div className='stat-box'>
+                    <h1>3/10 of bananas get eaten</h1>
+                </div>
+                <div className='stat-box'>
+                    <h1>4/10 of bananas get eaten</h1>
+                </div>
+            </div>
+            <button>Learn more</button>
+        </div>
+    )
+}; export default Statistics
