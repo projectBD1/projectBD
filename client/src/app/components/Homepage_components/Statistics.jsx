75995a3c6490daecd3f077bb73af1bc738958efb
Andy Zhou <azhou7@buffalo.edu>
2025-02-27 14:40:02 -0500

    Homepage Structure Complete

--- client/src/app/components/Homepage_components/Statistics.jsx
+++ client/src/app/components/Homepage_components/Statistics.jsx
@@ -1,23 +1,34 @@
-import '../Homepage_styles/Statistics.css'
+import '../Homepage_styles/Statistics.css';
+
 const Statistics = () => {
-    return(
+    return (
         <div className='statistics'>
-            <h1>What is Happening In Bangladesh?</h1>
-            <div className='stat-box-container'>
+            <h1 className='stat-header'>What is Happening In Bangladesh?</h1>
+            <div className='stat-container'>
                 <div className='stat-box'>
-                    <h1>1/10 of bananas get eaten</h1>
+                    <img className='stat-box-img' src='fire_in_hand.png'/>
+                    <div className='stat-box-text-container'>
+                        <h2>Statistic 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae libero dui.</p>
+                    </div>
                 </div>
                 <div className='stat-box'>
-                    <h1>2/10 of bananas get eaten</h1>
+                    <img className='stat-box-img' src='temporary_temple.webp'/>
+                    <div className='stat-box-text-container'>
+                        <h2>Statistic 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae libero dui.</p>
+                    </div>
                 </div>
                 <div className='stat-box'>
-                    <h1>3/10 of bananas get eaten</h1>
-                </div>
-                <div className='stat-box'>
-                    <h1>4/10 of bananas get eaten</h1>
+                    <img className='stat-box-img' src='temporary_temple2.webp'/>
+                    <div className='stat-box-text-container'>
+                        <h2>Statistic 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae libero dui.</p>
+                    </div>
                 </div>
             </div>
-            <button className='stat-button'>Learn more</button>
         </div>
-    )
-}; export default Statistics
+    );
+};
+
+export default Statistics;
