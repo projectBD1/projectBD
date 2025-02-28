dfd2388c305289072a3683ac2f192509559fa3f8
Andy Zhou <azhou7@buffalo.edu>
2025-02-28 01:20:01 -0500

    Homepage images changed

--- client/src/app/components/Homepage_components/Statistics.jsx
+++ client/src/app/components/Homepage_components/Statistics.jsx
@@ -5,21 +5,21 @@ const Statistics = () => {
         <div className='statistics'>
             <h1 className='stat-header'>What is Happening In Bangladesh?</h1>
             <div className='stat-container'>
-                <div className='stat-box'>
-                    <img className='stat-box-img' src='fire_in_hand.png'/>
-                    <div className='stat-box-text-container'>
-                        <h2>Statistic 1</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae libero dui.</p>
-                    </div>
-                </div>
-                <div className='stat-box'>
+                <div className='stat-box box1'>
                     <img className='stat-box-img' src='temporary_temple.webp'/>
                     <div className='stat-box-text-container'>
                         <h2>Statistic 1</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae libero dui.</p>
                     </div>
                 </div>
-                <div className='stat-box'>
+                <div className='stat-box box2'>
+                    <img className='stat-box-img' src='bangladeshprotest.avif'/>
+                    <div className='stat-box-text-container'>
+                        <h2>Statistic 1</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae libero dui.</p>
+                    </div>
+                </div>
+                <div className='stat-box box3'>
                     <img className='stat-box-img' src='temporary_temple2.webp'/>
                     <div className='stat-box-text-container'>
                         <h2>Statistic 1</h2>
@@ -27,6 +27,7 @@ const Statistics = () => {
                     </div>
                 </div>
             </div>
+            <button className='stat-button'>Learn More</button>
         </div>
     );
 };
