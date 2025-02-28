fda67433e41758492eb314b630d4681e3c9091cc
Andy Zhou <azhou7@buffalo.edu>
2025-02-28 01:20:01 -0500

    Homepage images changed

--- client/src/app/components/Homepage_components/Images.jsx
+++ client/src/app/components/Homepage_components/Images.jsx
@@ -4,15 +4,15 @@ const Images = () => {
         <div className='homepage-images'>
             <div className='homepage-images-main-container'>
                 <div className='homepage-images-small-container'>
-                    <img className='homepage-images-small' src='fire_in_hand.png'/>
-                    <img className='homepage-images-small' src='temporary_temple2.webp'/>
+                    <img className='homepage-images-small' src='fire_violence.webp'/>
+                    <img className='homepage-images-small' src='man_fleeing.avif'/>
                 </div>
-                <img className='homepage-images-tall' src='temporary_temple.webp'/>
+                <img className='homepage-images-tall' src='right_to_live.jpg'/>
                 <div className='homepage-images-small-container'>
-                    <img className='homepage-images-small' src='fire_in_hand.png'/>
-                    <img className='homepage-images-small' src='temporary_temple2.webp'/>
+                    <img className='homepage-images-small' src='military.webp'/>
+                    <img className='homepage-images-small' src='justice.webp'/>
                 </div>
-                <img className='homepage-images-tall' src='temporary_temple.webp'/>
+                <img className='homepage-images-tall' src='prayer.jpg'/>
             </div>
         </div>
     )
