5fe42856201adeb3652b792f2b661411849f953c
Andy Zhou <azhou7@buffalo.edu>
2025-03-01 13:40:50 -0500

    Improved Homepage mobile Responsiveness

--- client/src/app/components/Footer.jsx
+++ client/src/app/components/Footer.jsx
@@ -10,27 +10,30 @@ const Footer = () => {
                     <p>We imagine a future where Hindu and Muslim Children can play together.
                         A future where hate and violence does not divide us.</p>
                 </div>
-                <div className='footer2'>
-                    <ul className='footer-nav-links'>
-                        <h2>Navigation</h2>
-                        <li>Home</li>
-                        <li>About the Genocide</li>
-                        <li>Map & Report</li>
-                        <li>Blog</li>
-                        <li>Contact Us</li>
-                    </ul>
-                </div>
-                <div className='footer4'>
-                    <h2 >Support</h2>
-                    <div className='footer-support-text'>Help support Bangladesh Hindus</div>
-                    <div className='footer-support-text'>Donate to us or other organizations</div>
-                    <button className='footer-support-button'>Donate</button>
-                </div>
-                <div className='footer3'>
-                    <h2>Contact Us</h2>
-                    <div className='footer-contact-text'>Phone: 1(666)666-6666</div>
-                    <div className='footer-contact-text'>Email: contact@projectBD.org</div>
-                    <div className='footer-contact-text'>Mail: 123 Sesame Street, {<br/>} NY, NY 10010</div>
+                <div className='footer-container234'>
+
+                    <div className='footer2'>
+                        <ul className='footer-nav-links'>
+                            <h2>Navigation</h2>
+                            <li>Home</li>
+                            <li>About the Genocide</li>
+                            <li>Map & Report</li>
+                            <li>Blog</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div className='footer4'>
+                        <h2 >Support</h2>
+                        <div className='footer-support-text'>Help support Bangladesh Hindus</div>
+                        <div className='footer-support-text'>Donate to us or other organizations</div>
+                        <button className='footer-support-button'>Donate</button>
+                    </div>
+                    <div className='footer3'>
+                        <h2>Contact Us</h2>
+                        <div className='footer-contact-text'>Phone: 1(666)666-6666</div>
+                        <div className='footer-contact-text'>Email: contact@projectBD.org</div>
+                        <div className='footer-contact-text'>Mail: 123 Sesame Street, {<br/>} NY, NY 10010</div>
+                    </div>
                 </div>
             
             </div>
