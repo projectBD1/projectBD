4b6401bcd1fd98ceb28d4c8e223755f63f536ecd
MystProgrammer <animeshdeb89@gmail.com>
2025-03-15 23:10:28 -0400

    Update donate page styling

---
+++ client/src/app/Donate/Components/body.jsx
@@ -0,0 +1,11 @@
+import '../styles/donationStyle.css'
+export default function Body(){
+    return(
+        <div className="body">
+
+            <div className="tip1"><p>tip1</p></div>
+            <div className="tip2"><p>tip2</p></div>
+            <div className="tip3"><p>tip3</p></div>
+        </div>
+    )
+}
--- client/src/app/Donate/page.js
+++ client/src/app/Donate/page.js
@@ -2,19 +2,27 @@ import Payment from './payment';
 import Organizations from './Components/organizations';
 import Navbar from '../components/Navbar';
 import NormalNavbar from '../components/NormalNavbar';
-import './styles/page.css'
+import Header from '../Donate/Components/header';
+import Body from './Components/body';
+import './styles/page.css';
+
 export default function PayMe() {
   return (
-    <div className='pageContainer'>
+    <div className="pageContainer">
       {/* <h1>Standard pay</h1>
       <Payment/>
       <h1>Dynamic payment</h1>
       <DynamicPayment/> */}
-      <NormalNavbar/>
-      <Organizations/>
-      
-
-      
+      <NormalNavbar />
+      <Header />
+      <div className="bodyTitle">
+        <div className="subBodyTitle">
+          <h1>Expore Ways to Make an Impact</h1>
+          <p>Donate, Advocate, and Support Those in Need.</p>
+        </div>
+      </div>
+      <Body />
+      <Organizations />
     </div>
   );
 }
