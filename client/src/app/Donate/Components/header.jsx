b416125150ba9421dc6810eea4c0e76920769ebf
MystProgrammer <animeshdeb89@gmail.com>
2025-03-15 23:10:28 -0400

    Update donate page styling

---
+++ client/src/app/Donate/Components/header.jsx
@@ -0,0 +1,15 @@
+import '../styles/donationStyle.css';
+import helpingHands from '../images/helpinghands-6146693.jpg';
+import Image from 'next/image';
+
+
+export default function Header() {
+  return (
+    <div className='headerMaster'>
+      <div className="header">
+
+       Together, We Can Make A Real Difference.
+      </div>
+    </div>
+  );
+}
