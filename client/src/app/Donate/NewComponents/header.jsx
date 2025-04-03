3a8bf82d1ac0fe8645b0694c9d99bbb009340d67
MystProgrammer <animeshdeb89@gmail.com>
2025-04-02 21:43:08 -0400

    Update responsiveness. Now responsiveness across various display sizes.

---
+++ client/src/app/Donate/NewComponents/header.jsx
@@ -0,0 +1,151 @@
+'use client';
+import '../styles/header.css';
+import helpingHands from '../images/helpinghands-6146693.jpg';
+import Image from 'next/image';
+import { useState, useEffect, useRef } from 'react';
+import { initializePaddle } from '@paddle/paddle-js';
+
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [paddle, setPaddle] = useState();
+  const [value, setValue] = useState('');
+  const [priceName, setPriceName] = useState('');
+  const [priceDescription, setPriceDescription] = useState('');
+  const [productName, setProductName] = useState('');
+  const [productDescription, setProductDescription] = useState('');
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    initializePaddle({
+      environment: 'sandbox',
+      token: process.env.NEXT_PUBLIC_PADDLE_TEST_TOKEN,
+    }).then((paddle) => setPaddle(paddle)); //once instance is ready we set it in the state declared above
+  }, []);
+
+  
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    setVisible(true);
+  };
+
+
+  const handleCheckout = async (newvalue) => {
+    //if (!paddle) return alert('Paddle not intialized');
+    console.log('value is2: ', value);
+    const response = await fetch('http://localhost:3000/payment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        value: newvalue, 
+        priceName: priceName,
+        productName: productName,
+        priceDescription: priceDescription,
+        productDescription: productDescription,
+      }),
+    });
+    const data = await response.json();
+    paddle.Checkout.open({
+      transactionId: data.transaction,
+      settings: {
+        displayMode: 'overlay',
+        theme: 'dark',
+        successUrl: 'http://localhost:3000/success', //when payment us successful we redirect them to success url
+      },
+    });
+    // setValue('');
+    // setPriceDescription();
+    // setPriceName();
+    // setProductDescription();
+    // setProductName();
+  };
+
+
+  return (
+    <div className="headerMaster">
+      <div className="header">
+        <div className='call'>Make A Difference</div>
+        <div className="donateButton">
+          <button onClick={() => {
+                setIsOpen(true);
+                setPriceDescription('This is price description.');
+                setPriceName('This is price name.');
+                setProductDescription('This is prod description.');
+                setProductName('This is product name');
+              }}>Donate</button>
+               <div className={`popup ${isOpen ? 'open-popup' : ''}`} id="popup">
+              <div className="closeX">
+                <button onClick={() => setIsOpen(false)}>Close</button>
+              </div>
+              <h2>Choose an amount</h2>
+              <div className="paymentAmounts">
+                <button
+                  onClick={() => 
+                    handleCheckout('1.00')
+
+                   
+                    // setPriceDescription('This is price description.');
+                    // setPriceName('This is price name.');
+                    // setProductDescription('This is prod description.');
+                    // setProductName('This is product name');
+                  }
+                >
+                  $1.00
+                </button>
+                <button c
+                  onClick={() => 
+                    handleCheckout('3.00')
+                   
+                    // setPriceDescription('This is price description.');
+                    // setPriceName('This is price name.');
+                    // setProductDescription('This is prod description.');
+                    // setProductName('This is product name');
+                  }
+                >
+                  $3.00
+                </button>
+                <button
+                    onClick={() => 
+                      handleCheckout('5.00')
+                     
+                      // setPriceDescription('This is price description.');
+                      // setPriceName('This is price name.');
+                      // setProductDescription('This is prod description.');
+                      // setProductName('This is product name');
+                    }
+                >
+                  $5.00
+                </button>
+                <button
+                    onClick={() => 
+                      handleCheckout('7.00')
+                     
+                      // setPriceDescription('This is price description.');
+                      // setPriceName('This is price name.');
+                      // setProductDescription('This is prod description.');
+                      // setProductName('This is product name');
+                    }
+                >
+                  $7.00
+                </button>
+              </div>
+              <div className="inputAmount">
+                <p>Custom:</p>
+                <input
+                  onChange={handleChange}
+                  placeholder="Enter amount here."
+                />
+              </div>
+              {visible && (
+                <button className="closeBtn" onClick={()=>handleCheckout(value)}>
+                  Proceed to Checkout
+                </button>
+              )}
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/app/Donate/NewComponents/neworganizations.jsx
@@ -0,0 +1,81 @@
+import styles from '../styles/organizationsStyle.module.css';
+import Image from 'next/image';
+import sunflowerImg from '../images/sunflower-3304.jpg';
+import blueflowerImg from '../images/BlueFlower-985266.jpg';
+import redflowerImg from '../images/RedFlower-607013.jpg';
+
+
+export default function NewOrganizations() {
+  
+
+
+  return (
+    <div className={styles.organizations}>
+      <div className={styles.title}>
+        <h1>Our Partners</h1>
+      </div>
+      <div className={styles.orgs}>
+        <div className={styles.container}>
+          <div className={styles.iconImg}>
+            <Image
+              src={blueflowerImg}
+              className={styles.sunflowerImage}
+              alt="An image of a sunflower"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={styles.iconDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </div>
+          <div className={styles.iconLink}><button>Link</button></div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.iconImg}>
+            <Image
+              src={sunflowerImg}
+              className={styles.sunflowerImage}
+              alt="An image of a sunflower"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={styles.iconDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </div>
+          <div className={styles.iconLink}><button>Link</button></div>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.iconImg}>
+            <Image
+              src={redflowerImg}
+              className={styles.sunflowerImage}
+              alt="An image of a sunflower"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className={styles.iconDesc}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </div>
+          <div className={styles.iconLink}><button>Link</button></div>
+        </div>
+      </div>
+    </div>
+  );
+}
