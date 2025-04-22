bedef3a5796ac5c26507cf7ece9a5ca464eec0cc
MystProgrammer <animeshdeb89@gmail.com>
2025-04-22 18:25:20 -0400

    Donate page UI update, payment bug fix, payment UI update

--- client/src/app/Donate/NewComponents/neworganizations.jsx
+++ client/src/app/Donate/NewComponents/neworganizations.jsx
@@ -28,11 +28,7 @@ export default function NewOrganizations() {
           </div>
           <div className={styles.iconDesc}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
           </div>
           <div className={styles.iconLink}><button>Link</button></div>
         </div>
@@ -48,11 +44,7 @@ export default function NewOrganizations() {
           </div>
           <div className={styles.iconDesc}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. 
           </div>
           <div className={styles.iconLink}><button>Link</button></div>
         </div>
@@ -68,11 +60,7 @@ export default function NewOrganizations() {
           </div>
           <div className={styles.iconDesc}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </div>
           <div className={styles.iconLink}><button>Link</button></div>
         </div>
